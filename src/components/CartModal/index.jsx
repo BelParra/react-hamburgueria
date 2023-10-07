e66f06a3c3abf1b70379cd38097f3d3bf26b00c5
Bel <cibeleparra@rockemail.com>
2023-10-07 17:58:43 -0300

    feat: implementado modal e estilização da mesma e responsividade do header

--- src/components/CartModal/index.jsx
+++ src/components/CartModal/index.jsx
@@ -1,5 +1,8 @@
 import { MdClose } from "react-icons/md";
 import { CartItemCard } from "./CartItemCard";
+import styles from "./styles.module.scss";
+import "../../styles/index.scss";
+
 
 export const CartModal = ({ setVisible, cartList }) => {
    const total = cartList.reduce((prevValue, product) => {
@@ -7,26 +10,30 @@ export const CartModal = ({ setVisible, cartList }) => {
    }, 0);
 
    return (
-      <div role="dialog">
-         <div>
-            <h2>Carrinho de compras</h2>
-            <button onClick={() => setVisible(false)} aria-label="close" title="Fechar">
-               <MdClose size={21} />
-            </button>
-         </div>
-         <div>
-            <ul>
-               {cartList.map((product) => (
-                  <CartItemCard key={product.id} product={product} />
-               ))}
-            </ul>
-         </div>
-         <div>
-            <div>
-               <span>Total</span>
-               <span>{total.toLocaleString('pt-BR', { style: "currency", currency: "BRL" })}</span>
+      <div className={styles.modal} role="dialog">
+         <div className={styles.modalContent}>
+            <div className={styles.headerCart}>
+               <h2 className="Heading3">Carrinho de compras</h2>
+               <button onClick={() => setVisible(false)} aria-label="close" title="Fechar">
+                  <MdClose size={21} />
+               </button>
             </div>
-            <button>Remover todos</button>
+            <div>
+               <ul>
+                  {cartList.map((product) => (
+                     <CartItemCard key={product.id} product={product} />
+                  ))}
+               </ul>
+            </div>
+            <div className={styles.footerContent}>
+               <div className={styles.priceContent}>
+                  <span className="Headline">Total</span>
+                  <span>{total.toLocaleString('pt-BR', { style: "currency", currency: "BRL" })}</span>
+               </div>
+               <div className={styles.removeButton}>
+                  <button className="Headline">Remover todos</button>
+               </div>
+            </div>
          </div>
       </div>
    );
